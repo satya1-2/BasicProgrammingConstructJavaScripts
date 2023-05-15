d37375fe7bbbd47f23871390e6d0b71a4f449c2f
satya <122813843+satya1-2@users.noreply.github.com>
2023-05-15 23:07:11 +0530

    Delete selectionUsingIfAndElse directory
    selection using Switch case

--- selectionUsingIfAndElse/FindDayAndMonthGivenRange.js
+++
@@ -1,9 +0,0 @@
-const args = process.argv.slice(2);
-const day = parseInt(args[0]);
-const month = parseInt(args[1]);
-
-const isBetweenDates = (day >= 20 && month === 3) ||
-                      (month > 3 && month < 6) ||
-                      (month === 6 && day <= 20);
-
-console.log(isBetweenDates);
